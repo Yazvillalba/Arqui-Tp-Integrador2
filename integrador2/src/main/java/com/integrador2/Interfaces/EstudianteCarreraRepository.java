8bf5db43e477b7b9a8dcc2a642baec166b2016e6
Yazmin Villalba <yazminvillalba55@gmail.com>
2023-10-02 22:59:53 -0300

    acomodando codigo

--- integrador2/src/main/java/com/integrador2/Interfaces/EstudianteCarreraRepository.java
+++ integrador2/src/main/java/com/integrador2/Interfaces/EstudianteCarreraRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.integrador2.Entidades.Carrera;
 import com.integrador2.Entidades.Estudiante;
+import com.integrador2.Entidades.EstudianteCarrera;
 
 public interface EstudianteCarreraRepository {
-    
+   
+
     void matricularEstudiante(Estudiante estudiante, Carrera carrera);
     List<Estudiante> obtieneEstudiantesCarrera(Carrera carrera, String ciudad);
-
+    void agregarEstudianteCarrera(EstudianteCarrera EstudianteCarrera);
 }
